fd673ea223fcbd3114f793a23d8112b688989763
DrDestroydo <woodsy867@gmail.com>
2020-05-12 21:11:51 +0800

    More Testing

--- server.js
+++ server.js
@@ -24,9 +24,8 @@ app.use("/api/auth", require("./routes/auth"));
 
 //Serve static react in production
 if (process.env.NODE_ENV == "production") {
-	console.log("In Production!");
-	console.log(__dirname);
 	app.use(express.static(path.join(__dirname, "client/build")));
+	app.use("/assets", express.static(path.join(__dirname, "assets")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname + "/client/build/index.html"));
 	});
