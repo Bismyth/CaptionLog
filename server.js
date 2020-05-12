7c7e5bd2535a2c8f8f31919baad6e314092749a6
DrDestroydo <woodsy867@gmail.com>
2020-05-12 21:05:13 +0800

    Troubleshooting

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use("/api/auth", require("./routes/auth"));
 
 //Serve static react in production
 if (process.env.NODE_ENV == "production") {
+	console.log("In Production!");
 	app.use(express.static(path.join(__dirname, "client/build")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname + "/client/build/index.html"));
