4f37550e038336eaf2fa0a8fc9fe2a3deb133541
DrDestroydo <woodsy867@gmail.com>
2020-07-03 07:57:00 +0800

    fixing search bug

--- client/src/components/logStuff/Search.js
+++ client/src/components/logStuff/Search.js
@@ -20,9 +20,7 @@ const Search = (props) => {
     const [field, setField] = useState(props.match.params.field || "title");
     const [value, setValue] = useState(decodeURIComponent(search));
     useEffect(() => {
-        var updatedUrl = `${process.env.PUBLIC_URL}/search/${encodeURIComponent(
-            search
-        )}/${field}`;
+        var updatedUrl = `/search/${encodeURIComponent(search)}/${field}`;
         if (history.location.pathname !== updatedUrl) history.push(updatedUrl);
         const fetchData = async () => {
             setLoading(true);
