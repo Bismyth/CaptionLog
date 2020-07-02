d13eadd8c1a1c5c271664c6f32bb883baed68725
DrDestroydo <woodsy867@gmail.com>
2020-07-02 19:31:57 +0800

    testing backend

--- server.js
+++ server.js
@@ -12,30 +12,33 @@ app.use(express.json());
 
 //Connect to Server
 mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	})
-	.then(() => console.log("MongoDB Connected...."))
-	.catch((err) => console.error(err));
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB Connected...."))
+    .catch((err) => console.error(err));
 
 //Initialise Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/logs", require("./routes/logs"));
+app.use("/caption/api/logs", require("./routes/logs"));
+app.get("/api", (req, res) => {
+    res.send("Backend Working!");
+});
 
 //Serve static react in production
 if (process.env.NODE_ENV == "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
-	app.use("/assets", express.static(path.join(__dirname, "assets")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname + "/client/build/index.html"));
-	});
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use("/assets", express.static(path.join(__dirname, "assets")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
 }
 
 //Listen in on port and print what port
 app.set("port", process.env.PORT || 5000);
 app.listen(app.get("port"), () => {
-	console.log(`Server started on port ${app.get("port")}`);
+    console.log(`Server started on port ${app.get("port")}`);
 });
