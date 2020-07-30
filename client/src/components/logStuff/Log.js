12a87f2937e3c42b18fd6cf65c10a2abfc4c7c10
DrDestroydo <woodsy867@gmail.com>
2020-07-30 22:56:40 +0800

    Adding New Log renderer

--- client/src/components/logStuff/Log.js
+++ client/src/components/logStuff/Log.js
@@ -7,30 +7,27 @@ import {
     ListGroupItem,
     ListGroupItemHeading,
     ListGroupItemText,
-    Label,
+    Badge,
 } from "reactstrap";
 import { useSelector } from "react-redux";
 import "./scroll.css";
 import BackButton from "../BackButton";
+import logDisplay from "./LogDisplay.json";
+import Edit from "./actionButtons/Edit";
+import Delete from "./actionButtons/Delete";
+import { useHistory } from "react-router-dom";
 const Log = (props) => {
     const loggedIn = useSelector((state) => state.auth.isAuthenticated);
     const token = useSelector((state) => state.auth.token);
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [movie, setMovie] = useState(false);
-    const display = {
-        description: "Description:",
-        disks: "Disks:",
-        length: "Length:",
-        genre: "Genre:",
-        caption_source: "Caption Source:",
-        original_copy_location: "Original Copy Location:",
-        video_source: "Video Source:",
-        other: "Other: ",
-    };
+    const history = useHistory();
+    const { format, ratingColours } = logDisplay;
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
+            var result;
             var config = {
                 url: `${process.env.PUBLIC_URL}/api/logs/${props.match.params.id}`,
                 method: "get",
@@ -41,40 +38,141 @@ const Log = (props) => {
                 };
                 config.headers["x-auth-token"] = token;
             }
-            const result = await axios(config);
-            console.log(result.data);
-            setMovie(Object.keys(result.data.movieInfo).length > 0);
+            try {
+                result = await axios(config);
+            } catch (e) {
+                history.goBack();
+            }
+            if (result.data) setMovie(result.data.movieInfo !== undefined);
             setData(result.data);
             setLoading(false);
         };
         fetchData();
-    }, [props.match.params.id, token, loggedIn]);
-
+    }, [props.match.params.id, token, loggedIn, history]);
     return (
-        <div id="scroll">
-            <Container className="content">
-                {!loading ? (
-                    <Fragment>
-                        <div className="d-flex align-items-center mb-2">
-                            <BackButton className="mr-1" />
-                            <h2>
-                                {data.title +
-                                    (movie ? ` (${data.movieInfo.year})` : "")}
-                            </h2>
+        <Container className="content">
+            {!loading ? (
+                <Fragment>
+                    <div className="d-flex align-items-center mb-2">
+                        <BackButton className="mr-1" />
+                        <h2>
+                            {data.title}
+                            {movie ? ` (${data.movieInfo.year}) ` : ""}
+                            {movie ? (
+                                <Badge
+                                    style={{
+                                        backgroundColor: ratingColours[data.movieInfo.rating],
+                                    }}
+                                >
+                                    {data.movieInfo.rating}
+                                </Badge>
+                            ) : (
+                                <Fragment />
+                            )}
+                        </h2>
+                        <div className="ml-auto">
+                            <Edit id={data._id} />
+                            <Delete id={data._id} old={false} />
                         </div>
-                        {movie ? (
-                            <p>{`Rated ${data.movieInfo.rating}`}</p>
-                        ) : (
-                            <Fragment />
-                        )}
-                        <h2>Description</h2>
-                        <p>{data.description}</p>
-                    </Fragment>
-                ) : (
-                    <Spinner color="primary" />
-                )}
-            </Container>
-        </div>
+                    </div>
+                    {Object.entries(format).map(([key, [{ heading, multi }, ...entries]]) => {
+                        if (multi) {
+                            if (Object.keys(data).includes(key) && data[key].length > 0) {
+                                return (
+                                    <Fragment key={key}>
+                                        <h3 className="mt-2">{heading}</h3>
+                                        <ListGroup>
+                                            {data[key].map((value, i) => (
+                                                <ListGroupItem key={i}>
+                                                    {value.name ? (
+                                                        <ListGroupItemHeading>
+                                                            {value.name}
+                                                        </ListGroupItemHeading>
+                                                    ) : (
+                                                        <Fragment />
+                                                    )}
+                                                    <ListGroupItemText>
+                                                        {entries
+                                                            .filter((value) => {
+                                                                return value.align === "left";
+                                                            })
+                                                            .map((v, i) => {
+                                                                if (
+                                                                    Object.keys(value).includes(
+                                                                        v.value
+                                                                    )
+                                                                ) {
+                                                                    return (
+                                                                        <Fragment
+                                                                            key={`${key[0]}-${v.value}-${i}`}
+                                                                        >
+                                                                            <span>
+                                                                                {v.renderPart.replace(
+                                                                                    `{${v.value}}`,
+                                                                                    value[v.value]
+                                                                                )}
+                                                                            </span>{" "}
+                                                                            <br />
+                                                                        </Fragment>
+                                                                    );
+                                                                } else {
+                                                                    return (
+                                                                        <Fragment
+                                                                            key={`${key[0]}-${v.value}-${i}`}
+                                                                        />
+                                                                    );
+                                                                }
+                                                            })}
+                                                    </ListGroupItemText>
+                                                </ListGroupItem>
+                                            ))}
+                                        </ListGroup>
+                                    </Fragment>
+                                );
+                            } else {
+                                return <Fragment key={key} />;
+                            }
+                        } else {
+                            return (
+                                <Fragment key={key}>
+                                    {key !== "main" ? (
+                                        <h3 className="mt-2">{heading}</h3>
+                                    ) : (
+                                        <Fragment />
+                                    )}
+                                    <ListGroup>
+                                        {entries.map((v) => {
+                                            if (key === "main") var source = data;
+                                            else source = data[key];
+                                            if (Object.keys(source).includes(v.value)) {
+                                                return (
+                                                    <ListGroupItem key={`${key[0]}-${v.value}`}>
+                                                        <ListGroupItemHeading>
+                                                            {v.header}
+                                                        </ListGroupItemHeading>
+                                                        <ListGroupItemText>
+                                                            {v.special === "date"
+                                                                ? new Date(
+                                                                      source[v.value]
+                                                                  ).toDateString()
+                                                                : source[v.value]}
+                                                        </ListGroupItemText>
+                                                    </ListGroupItem>
+                                                );
+                                            } else {
+                                                return <Fragment key={`${key[0]}-${v.value}`} />;
+                                            }
+                                        })}
+                                    </ListGroup>
+                                </Fragment>
+                            );
+                        }
+                    })}
+                </Fragment>
+            ) : (
+                <Spinner color="primary" />
+            )}
+        </Container>
     );
 };
 
