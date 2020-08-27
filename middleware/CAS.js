45d60524434bf5a985c0d2d489f633e553159e5b
DrDestroydo <woodsy867@gmail.com>
2020-08-27 11:45:50 +0800

    silly

--- middleware/CAS.js
+++ middleware/CAS.js
@@ -1,8 +1,10 @@
 const CASAuthentication = require("node-cas-authentication");
 
-export const cas = new CASAuthentication({
+const cas = new CASAuthentication({
     cas_url: "https://sso.shenton.wa.edu.au/cas",
     service_url: "https://dec.shenton.wa.edu.au/caption-dev",
     return_to: "https://dec.shenton.wa.edu.au/caption-dev",
     session_info: "user",
 });
+
+module.exports = cas;
